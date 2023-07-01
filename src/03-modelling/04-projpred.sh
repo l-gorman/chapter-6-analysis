57bae53ac4eb1ca1a2626b52ea8d495930a03831
l-gorman <lgorman@turing.ac.uk>
2023-07-01 08:58:11 +0100

    shorten run time

--- src/03-modelling/04-projpred.sh
+++ src/03-modelling/04-projpred.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=7-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem=150G
 #SBATCH --account=sscm012844
 #SBATCH --array=1-2
